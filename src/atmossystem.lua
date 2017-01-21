d8a928bc7f1605fe0863a7971ee456db4479a351
C-gotoh <shigotoh1@gmail.com>
2017-01-22 00:10:00 +0100

    more party

--- src/atmossystem.lua
+++ src/atmossystem.lua
@@ -10,19 +10,24 @@ function atmossystem.load()
   atmossystem.bpm = 130
   atmossystem.timer = 0
   atmossystem.cooldown = 0
+
+  atmossystem.colortable = {}
 end
 
 function atmossystem.update(dt)
+  atmossystem.colortable.r = 128*(1+(math.sin(love.timer.getTime())))
+  atmossystem.colortable.g = 128*(1+(math.sin(2+love.timer.getTime())))
+  atmossystem.colortable.b = 128*(1+(math.sin(4+love.timer.getTime())))
   atmossystem.timer = atmossystem.timer + dt
   atmossystem.cooldown = atmossystem.cooldown - dt
   if atmossystem.timer > 60/130 then
-    love.graphics.setColor(255, 0, 0)
+    love.graphics.setColor(atmossystem.colortable.r, atmossystem.colortable.g, atmossystem.colortable.b)
 
     atmossystem.timer = 0
     atmossystem.cooldown = 0.2
   end
   if atmossystem.cooldown < 0 then
-    love.graphics.setColor(235, 153, 255)
+    love.graphics.setColor(235, 255, 255)
   end
 end
 
