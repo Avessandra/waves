d82a637d46fd9677bf2086f11f922067fe8a377a
C-gotoh <shigotoh1@gmail.com>
2017-01-21 23:51:39 +0100

    sum party action

--- main.lua
+++ main.lua
@@ -1,18 +1,21 @@
 local planets = require('src/planets')
 local narwhal = require('src/narwhal')
 local background = require('src/background')
+local atmossystem = require('src/atmossystem')
 
 function love.load()
 	world = love.physics.newWorld(0, 0, true)
 	planets.load()
 	narwhal.load()
 	background.load()
+	atmossystem.load()
 end
 
 function love.update( dt )
 	local speed = 100 * dt
 
 	world:update(dt)
+	atmossystem.update(dt)
 	planets.update(speed)
 	background.update(speed)
 	narwhal.update(dt, planets)
---
+++ src/atmossystem.lua
@@ -0,0 +1,29 @@
+local atmossystem = {}
+
+function atmossystem.load()
+  atmossystem.music = love.audio.newSource("assets/bassbeat.mp3") -- bpm 130
+  --atmossystem.music = love.audio.newSource("assets/space3.mp3") -- bpm 140
+  atmossystem.music:setLooping(true)
+  atmossystem.music:setVolume(0.2)
+  atmossystem.music:play()
+
+  atmossystem.bpm = 130
+  atmossystem.timer = 0
+  atmossystem.cooldown = 0
+end
+
+function atmossystem.update(dt)
+  atmossystem.timer = atmossystem.timer + dt
+  atmossystem.cooldown = atmossystem.cooldown - dt
+  if atmossystem.timer > 60/130 then
+    love.graphics.setColor(255, 0, 0)
+
+    atmossystem.timer = 0
+    atmossystem.cooldown = 0.2
+  end
+  if atmossystem.cooldown < 0 then
+    love.graphics.setColor(235, 153, 255)
+  end
+end
+
+return atmossystem
