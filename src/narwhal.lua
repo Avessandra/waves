b71dbc308df365e305cd7f2a610e3c4950243538
C-gotoh <shigotoh1@gmail.com>
2017-01-21 22:57:20 +0100

    somewhat working movement

--- src/narwhal.lua
+++ src/narwhal.lua
@@ -9,6 +9,7 @@ local rotation
 local velocity
 local frameWidth = 512
 local frameHeight = 256
+local rotspeed = 2
 
 function narwhal.load()
 		narwhal.reset()
@@ -33,19 +34,12 @@ function narwhal.update(dt, planets)
     end
 
     if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
-        pos_y = pos_y + 100*dt
+        rotation = rotation + rotspeed*dt
     end
 
     if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
-    	pos_y = pos_y - 100*dt
+    	rotation = rotation - rotspeed*dt
    	end
-    -- horizontal movement
-    if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
-	    pos_x = pos_x - 50*dt
-    end
-    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
-	    pos_x = pos_x + 50*dt
-    end
     if pos_y < -20 or pos_y > (love.graphics.getHeight()-40) then
         love.event.quit()
     end
@@ -57,21 +51,21 @@ function narwhal.update(dt, planets)
 			}
 			local distance = math.sqrt((planet.data.x - pos_x)^2 + (planet.data.y - pos_y)^2)
 			if distance <= planet.data.size_par*4 then
-				local narwhal_grad = math.atan2(narwhal_to_planet.y, narwhal_to_planet.x)-math.pi/2
+				local narwhal_grad = math.atan2(narwhal_to_planet.y, narwhal_to_planet.x)
 				local gravity = planet.data.size_par / distance^2
 				rotation = lerp(rotation, narwhal_grad, gravity)
-				--pos_x = pos_x + dt*narwhal_to_planet.x
-				--pos_y = pos_y + dt*narwhal_to_planet.y
+				velocity = velocity+gravity*50
 			end
 		end
-		pos_x = pos_x + ( math.cos(rotation)) *velocity* dt -velocity*dt
+		pos_x = pos_x + ( math.cos(rotation)) *velocity* dt - 100 * dt
 		pos_y = pos_y + ( math.sin(rotation)) *velocity* dt
 
 end
 
 
 function narwhal.draw()
-	love.graphics.draw(narwhal_im, frames[currentFrame], pos_x, pos_y, math.rad(-20)+rotation, 0.3, 0.3, frameWidth/2, frameHeight/2)
+	love.graphics.draw(narwhal_im, frames[currentFrame], pos_x, pos_y, math.rad(-10)+rotation, 0.3, 0.3, frameWidth/2, frameHeight/2)
+	love.graphics.circle("fill", pos_x, pos_y, 10)
 end
 
 function lerp(a, b, factor)
