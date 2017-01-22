4b7814601f48fec7fb4c58a7e0f5a188049ff176
Rafael Epplée <raffomania@gmail.com>
2017-01-22 13:11:53 +0100

    etter rules changes

--- main.lua
+++ main.lua
@@ -39,7 +39,7 @@ function love.update( dt )
 
 	rules_timer = rules_timer + dt
 
-	if rules_timer > 3 then
+	if rules_timer >= 4 * 60/ atmossystem.bpm then
 		current_rule = current_rule + 1
 		if current_rule > #rules then current_rule = 1 end
 		rules_timer = 0
